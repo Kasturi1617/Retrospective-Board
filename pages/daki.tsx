6c984d6de365fc377f1d423665b63e29d79287ad
Kasturi Sanyal <kasturisanyal3@gmail.com>
2025-11-22 20:42:14 +0530

    DAKi page title added

--- pages/daki.tsx
+++ pages/daki.tsx
@@ -1,23 +1,26 @@
-import Item from "../Components/item";
+import Title from "../Components/Title";
+import Item from "../Components/Item";
 import "./daki.css";
 
 function DAKI() {
     return (
-        <div className="container">
-            <div className="column">
-                <Item title="Drop" buttonTitle="Add Item" imagePath="down.png"></Item>
-            </div>
+        <div>
+            <div className="container">
+                <div className="column-title">
+                    <Title title="Drop" buttonTitle="Add Item" imagePath="down.png" columnTitle="Unproductive practices"></Title>
+                </div>
 
-            <div className="column">
-                <Item title="Add" buttonTitle="Add Item" imagePath="plus.png"></Item>
-            </div>
+                <div className="column-title">
+                    <Title title="Add" buttonTitle="Add Item" imagePath="plus.png" columnTitle="New ideas to implement"></Title>
+                </div>
 
-            <div className="column">
-                <Item title="Keep" buttonTitle="Add Item" imagePath="favorite.png"></Item>
-            </div>
+                <div className="column-title">
+                    <Title title="Keep" buttonTitle="Add Item" imagePath="favorite.png" columnTitle="Successful current practices"></Title>
+                </div>
 
-            <div className="column">
-                <Item title="Improve" buttonTitle="Add Item" imagePath="up-arrow.png"></Item>
+                <div className="column-title">
+                    <Title title="Improve" buttonTitle="Add Item" imagePath="up-arrow.png" columnTitle="Areas for enhancement"></Title>
+                </div>
             </div>
         </div>
     )
