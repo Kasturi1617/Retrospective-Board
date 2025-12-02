95578a8614e358bf8edbcf8a70e5760552fc0be0
Kasturi Sanyal <kasturisanyal3@gmail.com>
2025-12-02 23:59:50 +0530

    Header added in website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import { Providers } from "./provider";
 import Header from "@/Components/Header";
+import "./layout.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
-                
                 <Providers>
-                    <Header/>
-                    {children}
+                    <Header />
+                    <main className="page-body">
+                        {children}
+                    </main>
                 </Providers>
             </body>
         </html>
