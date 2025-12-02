3dc6ede46d06a519066c6ea90e7c583dc8bf24b5
Kasturi Sanyal <kasturisanyal3@gmail.com>
2025-12-02 23:22:37 +0530

    Added route middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { Providers } from "./provider";
+import Header from "@/Components/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
+                
                 <Providers>
+                    <Header/>
                     {children}
                 </Providers>
             </body>
